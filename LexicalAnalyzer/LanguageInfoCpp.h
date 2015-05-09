77388860d112f56bb9c11e3adf9868840ee87018
kverhun <k.verhun@gmail.com>
2015-05-09 21:20:37 +0300

    Cosmetic changes

--- LexicalAnalyzer/LanguageInfoCpp.h
+++ LexicalAnalyzer/LanguageInfoCpp.h
@@ -11,13 +11,13 @@ namespace LexicalAnalysis
 		enum ELexemeClasses : size_t
 		{
             LC_KEYWORD,
-            LC_IDENTIFIER = LC_KEYWORD + 1,
-            LC_INTEGER_LITERAL = LC_IDENTIFIER + 1,
+            LC_IDENTIFIER        = LC_KEYWORD + 1,
+            LC_INTEGER_LITERAL   = LC_IDENTIFIER + 1,
             LC_CHARACTER_LITERAL = LC_INTEGER_LITERAL + 1,
-            LC_FLOATING_LITERAL = LC_CHARACTER_LITERAL + 1,
-            LC_STRING_LITERAL = LC_FLOATING_LITERAL + 1,
-            LC_BOOLEAN_LITERAL = LC_STRING_LITERAL + 1,
-            LC_POINTER_LITERAL = LC_BOOLEAN_LITERAL + 1,
+            LC_FLOATING_LITERAL  = LC_CHARACTER_LITERAL + 1,
+            LC_STRING_LITERAL    = LC_FLOATING_LITERAL + 1,
+            LC_BOOLEAN_LITERAL   = LC_STRING_LITERAL + 1,
+            LC_POINTER_LITERAL   = LC_BOOLEAN_LITERAL + 1,
             //
             LC_PUNCTUATION_OR_OPERATOR = ILanguageInfo::SEPARATOR_LEXEME,
             LC_ERROR = ILanguageInfo::ERROR_LEXEME
